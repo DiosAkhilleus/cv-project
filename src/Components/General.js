ef81ba7cc6dc32a81ff6b43d062db0f217e6d08b
DiosAkhilleus <abertin1809@gmail.com>
2021-05-25 16:15:29 -0500

    react testing

--- src/Components/General.js
+++ src/Components/General.js
@@ -5,16 +5,21 @@ import '../App.css';
 function General() {
 
     const [updateActive, setUpdateActive] = useState (false);
+    const [text, setText] = useState ("false");
     const [first, setFirst] = useState("First Name");
+    const [active, setActive] = useState(false);
 
-    const changeFirst = () => {
-        console.log('click');
+    function update() {
+        setUpdateActive(!updateActive);
+        updateActive ? setText('true') : setText('false');
+        //console.log(updateActive);
     }
 
     
     return (
         <div>
-            
+            <h1 onClick={update}>{text}</h1>
+            <h1>General Info</h1>
             <input className='general-input' type='text' placeholder='First Name' />
             <input className='general-input' type='text' placeholder='Last Name' />
            
